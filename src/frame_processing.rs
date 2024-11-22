7764837c86b224b297b46987801656b91d59ab78
Shahram Barai <shahram.barai@gmail.com>
2024-11-22 17:02:09 +0200

    Added function for extracting the object from the frame based on a brightness threshlod

--- src/frame_processing.rs
+++ src/frame_processing.rs
@@ -1,5 +1,9 @@
 use anyhow::Result;
-use opencv::{core, imgproc, prelude::*};
+use opencv::{
+    core::{self, Point, Scalar},
+    imgproc,
+    prelude::*,
+};
 
 pub fn pixelate_frame(
     input: &Mat,
@@ -9,6 +13,10 @@ pub fn pixelate_frame(
     adjust_size_based_on_brightness: bool,
 ) -> Result<()> {
     let mut y = 0;
+    println!("input.rows() = {}", input.rows());
+    println!("input.cols() = {}", input.cols());
+    println!("y = {}", input.rows() / pixel_size);
+    println!("x = {}", input.cols() / pixel_size);
     while y <= input.rows() {
         let mut x = 0;
         while x <= input.cols() {
@@ -41,22 +49,22 @@ pub fn pixelate_frame(
     Ok(())
 }
 
-pub fn highlight_objects_with_contours(
-    input_frame: &Mat,
-    output_frame: &mut Mat,
-    filter_option: i32,
-    lower_threshold: f64,
-    upper_threshold: f64,
-) -> Result<()> {
+pub fn highlight_objects_with_contours(input_frame: &Mat, output_frame: &mut Mat) -> Result<()> {
     let mut edges = Mat::default();
 
-    if filter_option == 0 {
-        threshold_based_edge_detection(input_frame, &mut edges, lower_threshold)?;
-    } else if filter_option == 1 {
-        canny_operator(input_frame, &mut edges, lower_threshold, upper_threshold)?;
-    } else if filter_option == 2 {
-        scharr_operator(input_frame, &mut edges, lower_threshold, upper_threshold)?;
-    }
+    // Apply the canny algorithm to detect edges (steps: grayscale, blur, canny)
+    let mut gray = Mat::default();
+    imgproc::cvt_color(input_frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
+    let mut blurred = Mat::default();
+    imgproc::gaussian_blur(
+        &gray,
+        &mut blurred,
+        core::Size::new(5, 5),
+        0.0,
+        0.0,
+        core::BORDER_DEFAULT,
+    )?;
+    imgproc::canny(&blurred, &mut edges, 120.0, 255.0, 3, false)?;
 
     // Find contours
     let mut contours = core::Vector::<core::Vector<core::Point>>::new();
@@ -111,106 +119,36 @@ pub fn highlight_objects_with_contours(
     Ok(())
 }
 
-fn scharr_operator(
-    frame: &Mat,
-    edges: &mut Mat,
-    lower_threshold: f64,
-    upper_threshold: f64,
-) -> Result<()> {
+pub fn extract_object(
+    input_frame: &Mat,
+    brightness_threshold: f64,
+) -> Result<Vec<(Point, Scalar)>> {
+    // Convert the input frame to grayscale
     let mut gray = Mat::default();
-    imgproc::cvt_color(frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
+    imgproc::cvt_color(input_frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
 
-    let mut blurred = Mat::default();
-    imgproc::gaussian_blur(
-        &gray,
-        &mut blurred,
-        core::Size::new(5, 5),
-        0.0,
-        0.0,
-        core::BORDER_DEFAULT,
-    )?;
-
-    let mut thresholded = Mat::default();
+    // Create a binary image by thresholding the grayscale image
+    let mut mask = Mat::default();
     imgproc::threshold(
-        &blurred,
-        &mut thresholded,
-        lower_threshold,
-        upper_threshold,
+        &gray,
+        &mut mask,
+        brightness_threshold,
+        255.0,
         imgproc::THRESH_BINARY,
     )?;
 
-    let mut grad_x = Mat::default();
-    let mut grad_y = Mat::default();
-    let mut abs_grad_x = Mat::default();
-    let mut abs_grad_y = Mat::default();
-
-    imgproc::scharr(
-        &thresholded,
-        &mut grad_x,
-        core::CV_16S,
-        1,
-        0,
-        1.0,
-        0.0,
-        core::BORDER_DEFAULT,
-    )?;
-    imgproc::scharr(
-        &thresholded,
-        &mut grad_y,
-        core::CV_16S,
-        0,
-        1,
-        1.0,
-        0.0,
-        core::BORDER_DEFAULT,
-    )?;
-
-    core::convert_scale_abs(&grad_x, &mut abs_grad_x, 1.0, 0.0)?;
-    core::convert_scale_abs(&grad_y, &mut abs_grad_y, 1.0, 0.0)?;
-
-    core::add_weighted(&abs_grad_x, 0.5, &abs_grad_y, 0.5, 0.0, edges, core::CV_8U)?;
-
-    Ok(())
-}
-
-fn canny_operator(
-    frame: &Mat,
-    edges: &mut Mat,
-    lower_threshold: f64,
-    upper_threshold: f64,
-) -> Result<()> {
-    let mut gray = Mat::default();
-    imgproc::cvt_color(frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
-
-    let mut blurred = Mat::default();
-    imgproc::gaussian_blur(
-        &gray,
-        &mut blurred,
-        core::Size::new(5, 5),
-        0.0,
-        0.0,
-        core::BORDER_DEFAULT,
-    )?;
-
-    imgproc::canny(&blurred, edges, lower_threshold, upper_threshold, 3, false)?;
-    Ok(())
-}
-
-fn threshold_based_edge_detection(frame: &Mat, edges: &mut Mat, threshold: f64) -> Result<()> {
-    let mut gray = Mat::default();
-    imgproc::cvt_color(frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
-
-    let mut blurred = Mat::default();
-    imgproc::gaussian_blur(
-        &gray,
-        &mut blurred,
-        core::Size::new(5, 5),
-        0.0,
-        0.0,
-        core::BORDER_DEFAULT,
-    )?;
-
-    imgproc::threshold(&blurred, edges, threshold, 255.0, imgproc::THRESH_BINARY)?;
-
-    Ok(())
+    // Extract coordinates and pixel values of the object
+    let mut object_pixels = Vec::new();
+    for y in 0..mask.rows() {
+        for x in 0..mask.cols() {
+            if *mask.at_2d::<u8>(y, x)? == 0 {
+                let color = input_frame.at_2d::<core::Vec3b>(y, x)?;
+                object_pixels.push((
+                    Point::new(x, y),
+                    Scalar::new(color[0] as f64, color[1] as f64, color[2] as f64, 0.0),
+                ));
+            }
+        }
+    }
+    Ok(object_pixels)
 }
