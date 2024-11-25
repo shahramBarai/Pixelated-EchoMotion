3cf48fd7a260657c5784e8a71dac78c071b42cad
Shahram Barai <shahram.barai@gmail.com>
2024-11-25 18:26:33 +0200

    Add particle system (Particle and Effect class)

---
+++ src/particle_system/particle_system.rs
@@ -0,0 +1,159 @@
+use anyhow::Result; // Automatically handle the error types
+use opencv::{
+    core::{self, Point, Rect, Scalar},
+    imgproc,
+    prelude::*,
+};
+
+pub struct Particle {
+    window_size: Point,
+    origin: Point,
+    size: i32,
+    color: core::Scalar,
+    x: f64,
+    y: f64,
+    vx: f64,
+    vy: f64,
+}
+
+impl Particle {
+    pub fn new(window_size: Point, origin: Point, size: i32, color: core::Scalar) -> Self {
+        Particle {
+            window_size,
+            origin,
+            size,
+            color,
+            x: origin.x as f64,
+            y: origin.y as f64,
+            vx: 0.0,
+            vy: 0.0,
+        }
+    }
+
+    pub fn draw(&self, frame: &mut Mat) -> Result<()> {
+        imgproc::rectangle(
+            frame,
+            Rect::new(self.x as i32, self.y as i32, self.size, self.size),
+            self.color,
+            -1,
+            imgproc::LINE_8,
+            0,
+        )?;
+        Ok(())
+    }
+
+    pub fn update(&mut self, mouse_coords: Point) {
+        // Influence by mouse
+        let dx = mouse_coords.x as f64 - self.x;
+        let dy = mouse_coords.y as f64 - self.y;
+        let distance = dx * dx + dy * dy;
+        let force;
+        if distance == 0.0 {
+            force = 0.0;
+        } else {
+            force = -5000.0 / distance;
+        }
+
+        if distance < 5000.0 {
+            let angel = dy.atan2(dx) as f64;
+            self.vx += force * angel.cos() as f64;
+            self.vy += force * angel.sin() as f64;
+        }
+
+        self.vx *= 0.80;
+        self.vy *= 0.80;
+
+        self.x += (self.origin.x as f64 - self.x) * 0.05 + self.vx;
+        self.y += (self.origin.y as f64 - self.y) * 0.05 + self.vy;
+
+        if (self.origin.x as f64 - self.x).abs() < 1.0
+            && (self.origin.y as f64 - self.y).abs() < 1.0
+        {
+            self.x = self.origin.x as f64;
+            self.y = self.origin.y as f64;
+        }
+
+        if self.x < 0.0 {
+            self.x = 0.0;
+        }
+        if self.y < 0.0 {
+            self.y = 0.0;
+        }
+        if self.x > self.window_size.x as f64 {
+            self.x = self.window_size.x as f64;
+        }
+        if self.y > self.window_size.y as f64 {
+            self.y = self.window_size.y as f64;
+        }
+    }
+}
+
+pub struct Effect {
+    pub window_size: Point,
+    pub pixel_size: i32,
+    pub pixel_spacing: i32,
+    pub brightness_threshold: f64,
+    particles: Vec<Particle>,
+    pub mouse_coords: Point,
+}
+
+impl Effect {
+    pub fn new(window_width: i32, window_height: i32, pixel_size: i32, pixel_spacing: i32) -> Self {
+        Effect {
+            window_size: Point::new(window_width, window_height),
+            pixel_size,
+            pixel_spacing,
+            brightness_threshold: 200.0,
+            particles: Vec::new(),
+            mouse_coords: Point::new(0, 0),
+        }
+    }
+
+    pub fn init(&mut self, input_frame: &Mat) -> Result<()> {
+        // Convert the input frame to grayscale
+        let mut gray = Mat::default();
+        imgproc::cvt_color(input_frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
+
+        // Create a binary image by thresholding the grayscale image
+        let mut mask = Mat::default();
+        imgproc::threshold(
+            &gray,
+            &mut mask,
+            self.brightness_threshold,
+            255.0,
+            imgproc::THRESH_BINARY,
+        )?;
+
+        let mut y = 0;
+        while y < mask.rows() {
+            let mut x = 0;
+            while x < mask.cols() {
+                if *mask.at_2d::<u8>(y, x)? == 0 {
+                    let color = input_frame.at_2d::<core::Vec3b>(y, x)?;
+                    self.particles.push(Particle::new(
+                        self.window_size,
+                        Point::new(x, y),
+                        self.pixel_size,
+                        Scalar::new(color[0] as f64, color[1] as f64, color[2] as f64, 0.0),
+                    ));
+                }
+                x += self.pixel_size + self.pixel_spacing;
+            }
+            y += self.pixel_size + self.pixel_spacing;
+        }
+        Ok(())
+    }
+
+    pub fn draw(&self, frame: &mut Mat) -> Result<()> {
+        for particle in &self.particles {
+            particle.draw(frame)?;
+        }
+        Ok(())
+    }
+
+    pub fn update(&mut self) {
+        for particle in &mut self.particles {
+            particle.update(self.mouse_coords);
+        }
+    }
+}
