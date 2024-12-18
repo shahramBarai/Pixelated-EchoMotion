9f378e3763439327ce87a3b892da25e28b7d8e78
Shahram Barai <shahram.barai@gmail.com>
2024-12-18 16:01:47 +0200

    Refactor main function for improved async processing; streamline video source initialization, frame handling, and performance measurement

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ use frame_processing::FrameProcessor;
 use gui_interaction::Window;
 use video_capture::VideoSource;
 
-use std::sync::{Arc, Mutex};
+use std::{
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 use anyhow::{Ok, Result}; // Automatically handle the error types
 use particle_system::{EffectType, ParticleSystem}; // Import the ParticleSystem and EffectType
@@ -22,7 +25,7 @@ use opencv::{
 use tokio::task;
 
 // Define the constants
-const PIXEL_SIZE: i32 = 5; // Define maximum possible pixel size
+const PIXEL_SIZE: i32 = 1; // Define maximum possible pixel size
 const PIXEL_SPACING: i32 = 0; // Define the spacing between pixels
 const WINDOW_NAME: &str = "Window"; // Define the name of the window
 const WINDOW_WIDTH: i32 = 960; // Define the width of the window
@@ -83,6 +86,9 @@ fn detect_interference(point_1: Point, point_2: Point, output: &mut Mat) -> Resu
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Initialize the logger
+    env_logger::init();
+
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         println!(
@@ -93,24 +99,24 @@ async fn main() -> Result<()> {
     }
 
     // Initialize the video sources
-    let mut video_source_0 = VideoSource::new()?;
-    video_source_0.set_source_file(&args[2])?;
-    video_source_0.set_resolution(VIDEO_RESOLUTION_WIDTH, VIDEO_RESOLUTION_HEIGHT)?;
-    video_source_0.update_frame()?;
-
     let mut video_source_1 = VideoSource::new()?;
-    video_source_1.set_source_file(&args[3])?;
+    video_source_1.set_source_file(&args[2])?;
     video_source_1.set_resolution(VIDEO_RESOLUTION_WIDTH, VIDEO_RESOLUTION_HEIGHT)?;
     video_source_1.update_frame()?;
 
+    let mut video_source_2 = VideoSource::new()?;
+    video_source_2.set_source_file(&args[3])?;
+    video_source_2.set_resolution(VIDEO_RESOLUTION_WIDTH, VIDEO_RESOLUTION_HEIGHT)?;
+    video_source_2.update_frame()?;
+
     // Initialize the particle system effect
     let mut particle_system = ParticleSystem::new(
-        video_source_0.frame.size()?,
+        video_source_1.frame.lock().unwrap().size()?,
         PIXEL_SIZE,
         PIXEL_SPACING,
         OBJECTS_INTERFERENCE_DISTANCE * 1000,
     );
-    particle_system.init(&video_source_0.frame, 2)?;
+    particle_system.init(&video_source_1.frame.lock().unwrap(), 2)?;
 
     // Initialize the frame processor
     let mut frame_processor = FrameProcessor::new(PIXEL_SIZE, PIXEL_SPACING, 200.0);
@@ -125,49 +131,101 @@ async fn main() -> Result<()> {
     let mut point_2 = Point::new(0, 0);
 
     loop {
+        // Measure loop start time
+        let loop_start = std::time::Instant::now();
+
         // Update the frames from the video sources
-        if !video_source_0.update_frame()? {
+        if !video_source_1.update_frame()? {
             break;
         }
-        if !video_source_1.update_frame()? {
+        if !video_source_2.update_frame()? {
             break;
         }
 
         // Clear output frame
         particle_system.clean_output_frame()?;
 
+        // Access frames without cloning
+        let frame1 = {
+            let locked_frame = video_source_1.frame.lock().unwrap();
+            Arc::clone(&Arc::new(locked_frame.clone()))
+        };
+        let frame2 = {
+            let locked_frame = video_source_2.frame.lock().unwrap();
+            Arc::clone(&Arc::new(locked_frame.clone()))
+        };
+
         // Convert the frames to grayscale (black and white)
-        frame_processor.convert_to_grayscale(&video_source_0.frame, 0)?;
-        frame_processor.convert_to_grayscale(&video_source_1.frame, 1)?;
+        frame_processor.convert_to_grayscale(&frame1, 0)?;
+        frame_processor.convert_to_grayscale(&frame2, 1)?;
 
         // Find the contours of the objects in the frames
         frame_processor.find_object_contour(0)?;
         frame_processor.find_object_contour(1)?;
+
+        // Measure frame processing time
+        let frame_processing_time = std::time::Instant::now() - loop_start;
 
         // Draw the contours of the objects in the frames
         // frame_processor.draw_contours(&mut particle_system.output_frame)?;
 
         (point_1, point_2) = frame_processor.find_closest_points(0, 1).await?;
 
-        // Draw the closest points in the frames
+        // Measure the closest points calculation time
+        let closest_points_time = std::time::Instant::now() - loop_start - frame_processing_time;
+        let mut extract_object_time = std::time::Duration::new(0, 0);
+        let mut add_object_time = std::time::Duration::new(0, 0);
+
+        // Update the particle system
         if particle_system.get_animation_status(0)? {
             particle_system.update(point_2).await?;
             object_2 = frame_processor.extract_object(1).await?;
-            particle_system.add_object(&video_source_1.frame, &object_2, 1)?;
+
+            // Add the object to the particle system
+            extract_object_time = std::time::Instant::now()
+                - loop_start
+                - frame_processing_time
+                - closest_points_time;
+
+            particle_system.add_object(&frame2, &object_2, 1).await?;
+
+            // Measure the add object time
+            add_object_time = std::time::Instant::now()
+                - loop_start
+                - frame_processing_time
+                - closest_points_time
+                - extract_object_time;
         } else {
             // Extract the objects from the frames (black pixels)
             object_1 = frame_processor.extract_object(0).await?;
             object_2 = frame_processor.extract_object(1).await?;
 
+            // Measure the extract object time
+            extract_object_time = std::time::Instant::now()
+                - loop_start
+                - frame_processing_time
+                - closest_points_time;
+
             // Add the objects to the particle system
-            particle_system.add_object(&video_source_0.frame, &object_1, 0)?;
-            particle_system.add_object(&video_source_1.frame, &object_2, 1)?;
+            particle_system.add_object(&frame1, &object_1, 0).await?;
+            particle_system.add_object(&frame2, &object_2, 1).await?;
+
+            // Measure the add object time
+            add_object_time = std::time::Instant::now()
+                - loop_start
+                - frame_processing_time
+                - closest_points_time
+                - extract_object_time;
 
             if detect_interference(point_1, point_2, &mut particle_system.output_frame)? {
                 particle_system.set_animation_status(0, true);
                 particle_system.set_effect_type(0, EffectType::Push);
             }
         }
+
+        // Measure the particle system update time
+        let particle_system_update_time =
+            std::time::Instant::now() - loop_start - closest_points_time;
 
         // Show the output frame in the window
         particle_system.draw()?;
@@ -179,8 +237,15 @@ async fn main() -> Result<()> {
             break;
         }
 
-        // Sleep for a short duration to avoid high CPU usage
-        std::thread::sleep(std::time::Duration::from_millis(1));
+        // Measure the total loop time
+        let loop_time = std::time::Instant::now() - loop_start;
+        println!(
+            "Extract object: {:?}, Add object: {:?}, Particle system update: {:?}, Loop: {:?}",
+            extract_object_time, add_object_time, particle_system_update_time, loop_time
+        );
+
+        // Sleep asynchronously to avoid high CPU usage
+        tokio::time::sleep(Duration::from_millis(1)).await;
     }
     Ok(())
 }
